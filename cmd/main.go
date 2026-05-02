02f020dc269d6a545e65f65e6614365029ceff80
Nikita Kotkov <nikitakotkov66@gmail.com>
2026-05-03 00:35:45 +0300

    Настройка миграций БД

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"gpwd/config"
+	"gpwd/internal/database"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	conf := config.New()
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", conf.DB.Host, conf.DB.Port, conf.DB.User, conf.DB.Password, conf.DB.DBName, conf.DB.SSLMode)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("DB connected")
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(5)
+
+	if err := database.Migrate(db); err != nil {
+		log.Printf("Migration warning: %v", err)
+	} else {
+		log.Println("migrated succesfully")
+	}
+	
 	r := gin.Default()
 
 	r.GET("/health", func(c *gin.Context) {
